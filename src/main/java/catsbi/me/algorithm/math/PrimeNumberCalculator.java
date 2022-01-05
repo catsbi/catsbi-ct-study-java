81e416ef1a1a980d1b4f82ac696af7efe853568f
catsbi <killfine198@gmail.com>
2022-01-05 18:04:20 +0900

    feat: 유클리드 알고리즘, 에로토스테네스의 체 알고리즘 구현

---
+++ src/main/java/catsbi/me/algorithm/math/PrimeNumberCalculator.java
@@ -0,0 +1,45 @@
+package catsbi.me.algorithm.math;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+/**
+ * 에라토스의 체를 이용한 소수 구하기 알고리즘
+ */
+
+public class PrimeNumberCalculator {
+    static int[] arr;
+
+    public static void printCalculate(int length) {
+        fill(length);
+        filtering();
+        print();
+    }
+
+    private static void fill(int length) {
+        arr = new int[length + 1];
+        for (int i = 2; i <= length; i++) {
+            arr[i] = i;
+        }
+    }
+
+    private static void filtering() {
+        for (int i = 2; i < arr.length; i++) {
+            if (arr[i] != 0) {
+                for (int j = 2 * i; j < arr.length; j += i) {
+                    arr[j] = 0;
+                }
+            }
+        }
+    }
+
+    private static void print() {
+        String result = Arrays.stream(arr)
+                .filter(num -> num != 0)
+                .mapToObj(String::valueOf)
+                .collect(Collectors.joining(","));
+
+        System.out.printf("[%s]", result);
+    }
+
+}
